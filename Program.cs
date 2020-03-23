80c9e1e3d7ce396110c0fd783b7d86a9377730ad
Ahmad Rais <ahmad.rdj2@gmail.com>
2020-03-23 22:34:26 +0700

    membuat class

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver taxi = new Driver();
+
+            taxi.DriverName = "Beben";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
